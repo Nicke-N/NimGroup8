eb89c678000328163ce97e2ed46a982710023804
ashurw <ashurw@gmail.com>
2019-09-25 14:47:45 +0200

    P till DIV man kan ta bort sticks men blir lite fel mot slutet

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+let allsticks = [];
+
 function startgame() {
 
     let player1 = document.getElementById("player1"); //hämtar textrutor som ej syns
@@ -14,24 +16,31 @@ function startgame() {
         document.getElementById("players").classList.remove("hide"); //visar en annan
         console.log("woho");
 
-        let gameboard = document.createElement("p"); //skapar P som ska innehålla gameboard
+        let gameboard = document.createElement("div"); //skapar DIV som ska innehålla gameboard
         gameboard.id = "dispSticks";
-        let gamenav = document.createElement("p"); //P som ska innehålla navigation, gamenav
-        let gamelog = document.createElement("p");
+        let gamenav = document.createElement("div"); //DIV som ska innehålla navigation, gamenav
+        let gamelog = document.createElement("div");
         let alloptions = document.createElement("select"); //samlar alla options nedan
         let log = document.createElement("textarea");   //log skpaas o innehåller textarea
         log.id = "gamelog"; //log for id, log:en ska visa vems tur det är, vem som gjort vad osv.
         log.setAttribute("rows", "20"); // log for rows
         log.setAttribute("cols", "30"); //log for cols
-        let allsticks = [];
 
         for (let eachstick = 0; eachstick < 21; eachstick++) { //fyller array med sticks
-            allsticks[eachstick] = "|";
+            let theStick = document.createElement("div")
+            theStick.className = "stick"
+            theStick.id = "sNr" + eachstick;
+            // theStick.innerText = "|";
+            allsticks[eachstick] = theStick;
         }
-        gameboard.textContent = allsticks.join(" "); //ersätter "," i array med " "
+        console.log(allsticks);
+        for (let i of allsticks) {
+            gameboard.appendChild(i);
+        }
 
         for (let eachoption = 1; eachoption < 4; eachoption++) {
             let specificoption = document.createElement("option"); //skapar options 1 - 3
+            specificoption.id = "op" + eachoption;
             specificoption.textContent = eachoption;
             specificoption.value = eachoption; //tror ej denna behövs men lämnar den här ifall ni vill använda den
             alloptions.appendChild(specificoption); // alloptions(select) hämtar options en i taget
@@ -51,19 +60,20 @@ function startgame() {
         gamecontent.appendChild(gameboard); // gamecontent hämtar gameboard och gamenav.
         gamecontent.appendChild(gamenav);
         gamecontent.appendChild(gamelog);
-        localStorage.setItem("theboard", allsticks); //sparar arrayen.
+        // localStorage.setItem("theboard", allsticks); //sparar arrayen.
     } else {
         console.log("oops"); //användes för felsökning                                        
     }
 }
 
 function draw() {
+    
     let button = document.getElementById("play"); //hämtar knapp med id
-    let allsticks = localStorage.getItem("theboard"); //hämtar array med sticks
+    // let allsticks = localStorage.getItem("theboard"); //hämtar array med sticks
     
     if(button.value == 1) { //om knappen har 1 i value, som under p1's tur, händer detta
 
-
+        removeStick();
         document.getElementById("player1").classList.remove("playing");
         document.getElementById("player2").classList.add("playing");
         button.value = 2;   //efter turen blir button value 2, vilket e p2s tur.
@@ -71,7 +81,7 @@ function draw() {
 
     } else if(button.value == 2){
 
-
+        removeStick();
         document.getElementById("player1").classList.add("playing");
         document.getElementById("player2").classList.remove("playing");
         button.value = 1;
@@ -79,5 +89,19 @@ function draw() {
     }
 
  
+
+}
+
+// function tar bort stickor, kollar först vad man har valt
+function removeStick() {
+    let e = document.getElementById("options");
+    var removeStickValue = Number(e.options[e.selectedIndex].value);
+    console.log(removeStickValue);
+    let gameboard = document.getElementById("dispSticks");
+
+    for (let i = 0; i < removeStickValue; i++) {
+        gameboard.removeChild(gameboard.childNodes[i]);
+        console.log(i);
+    }
 
 }
